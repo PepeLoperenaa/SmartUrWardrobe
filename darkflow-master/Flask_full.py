939a390f963d9a318906fabd6d7f04405420b3bd
Hardnamedagain133433 <major98mate@gmail.com>
2019-06-27 13:09:25 +0200

    First part of biggest commit

---
+++ darkflow-master/Flask_full.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+import cv2
+from darkflow.net.build import TFNet
+import matplotlib.pyplot as plt
+import os
+
+app = Flask(__name__)
+
+options = {
+    'model': 'cfg/yolo-voc-c1.cfg',##linking of the config file
+    'load': 76260,##number of the checkpoint to load the weights of the model from
+    'threshold': 0.5##limit of confidence in the recognition of object
+}
+
+tfnet = TFNet(options)##runs tf with the above specified settings
+
+@app.route('/')
+def index():
+	return 'placeholder page to check connection'
+
+@app.route('/upload', methods= ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['image']##requests the files defined as image
+        f.save('uploads/newTest.jpg')##saves the file sent to the location with the given name
+        return 'success'
+    else:
+        return 'failiure'
+		
+@app.route('/main')
+def img2():
+	img = cv2.imread('uploads/newTest.jpg')##read the images specifics through cv2
+	result = tfnet.return_predict(img)##uses the running model to predict if the image contains a wheelchair
+	isWheelchair = False
+	
+	if len(result) > 0:##check the length of the result to see if there's a wheelchair on the image
+		isWheelchair = True
+		
+	if isWheelchair == True:
+		return 'Wheelchair detected'
+	else:
+		return 'No wheelchair detected'	
+		
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
